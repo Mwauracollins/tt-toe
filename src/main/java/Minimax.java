4aeffc216268b10c9f3f3c8c46bec1b340e484a2
Mwauracollins <collinsmwaura8844@gmail.com>
2024-01-27 17:33:15 +0300

    For the AI player

---
+++ src/main/java/Minimax.java
@@ -0,0 +1,12 @@
+public class Minimax {
+    //For the ai player.
+    //TODO Consider the depth
+
+    /**
+     * methods:
+     *  finding the best move
+     *  the minimax function
+     *  checking the winner
+     *
+     */
+}
